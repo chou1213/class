6c382d47c2ca15a651848a375f21df4782e7151c
joanChen <joan.zhou2@gmail.com>
2021-01-09 00:29:05 +0800

    4-2

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 3. src/model/test.js 编辑users集合模型
 4. src/model/demo.js 引用users数据模型，对数据进行增删改成（npx babel-node src/model/demo.js ）
 5. npm i -S redis 安装redis
+6. src/config/RedisConfig redis连接配置，设置set，get，hset，hgetall
+7. src/config/test.js 调用方法操作redis
--- src/config/test.js
+++ src/config/test.js
@@ -1,7 +1,13 @@
-import { getValue, setValue } from './test';
+import { getValue, setValue, getHValue } from './RedisConfig';
 
-setValue('imooc', 'immoc test');
+setValue('imooc', 'immoc test1');
 
 getValue('imooc').then(res => {
-    console.log(res);
+	console.log('getValue', res);
+})
+
+setValue('obj', { name: "jojo", age: 30 });
+
+getHValue('obj').then(res => {
+	console.log('getHValue', res);
 })
